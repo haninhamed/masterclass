a4e43d887c74fc2a00573cc750c9ab583b1c467a
haninhamed <hanin@krpt.co.uk>
2017-01-09 10:57:16 +0000

    release/9
    + added charts
    + added views module
    + updated scripts

--- sites/all/themes/mc_theme/scripts/scripts.js
+++ sites/all/themes/mc_theme/scripts/scripts.js
@@ -479,5 +479,38 @@
 	// 	}
 	// };
 
+// Draw pie charts
+
+	Drupal.behaviors.pieCharts = {
+
+		attach: function () {
+			function $$(selector, context) {
+context = context || document;
+var elements = context.querySelectorAll(selector);
+return Array.prototype.slice.call(elements);
+}
+
+			$(document).ready(function() {
+				$$('.pie').forEach(function(pie) {
+					var p = parseFloat(pie.textContent);
+					var NS = "http://www.w3.org/2000/svg";
+					var svg = document.createElementNS(NS, "svg");
+					var circle = document.createElementNS(NS, "circle");
+					var title = document.createElementNS(NS, "title");
+					circle.setAttribute("r", 16);
+					circle.setAttribute("cx", 16);
+					circle.setAttribute("cy", 16);
+					circle.setAttribute("stroke-dasharray", p + " 100");
+					svg.setAttribute("viewBox", "0 0 32 32");
+					title.textContent = pie.textContent;
+					pie.textContent = '';
+					svg.appendChild(title);
+					svg.appendChild(circle);
+					pie.appendChild(svg);
+				});
+			});
+		}
+	};
+
 }(jQuery));
 
